7e8115afa58d097fbc80fb54f574d6414ba2c473
Patricia <patricia.bildea@gmail.com>
2021-06-17 19:03:25 +0300

    static methods added

---
+++ Enemies.py
@@ -0,0 +1,25 @@
+class Enemy:
+    def __init__(self, health, damage):
+        self.health = health
+        self.damage = damage
+
+    def attack(self):
+        pass
+
+
+class Goblin(Enemy):
+    def __init__(self):
+        super(Goblin, self).__init__(100, 5)
+        print("Glbglbglb, I am a goblin and will slash you")
+
+
+class Rat(Enemy):
+    def __init__(self):
+        super(Rat, self).__init__(100, 10)
+        print("I am a super rat and will eat your brains")
+
+
+class Orc(Enemy):
+    def __init__(self):
+        super(Orc, self).__init__(100, 15)
+        print("Wroooaarr, I am an orc and will crush you")
