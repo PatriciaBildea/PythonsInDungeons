a2761a2f93e6d41d28f8d08005d9e3c992247840
Patricia <patricia.bildea@gmail.com>
2021-06-17 21:24:11 +0300

    Made code nicer Added exploring sound Added fight

--- Utils.py
+++ Utils.py
@@ -1,17 +1,33 @@
 import os
-import random
 import winsound
 import Player
 import Enemies
 import random
 
 
-@staticmethod
-def path():
-    print("Entering the world...")
-    input("Press any key to continue")
-    os.system("cls")
-    path = input('''A crossroads lies ahead of you...
+class Messages:
+    @staticmethod
+    def intro_message():
+        introMsg = """ 
+*****************************************************************************************
+* Welcome, stranger!                                                                    *
+* Here in Hinterlands, you'll get to fight dragons and conquer the deadliest dungeons.  *
+* In a country where magic rules, anything is possible if you wish so.                  *
+* It all depends on you, brave hero.                                                    *
+*****************************************************************************************
+        """
+        print(introMsg)
+
+
+class Choices:
+    @staticmethod
+    def path():
+        print("Entering the world...")
+        input("Press any key to continue")
+        os.system("cls")
+        sound = "Exploring.wav"
+        winsound.PlaySound(sound, winsound.SND_ASYNC)
+        print('''A crossroads lies ahead of you...
                       (  )   /  ^  /
                 (  ) (    ) /  /  /
                (    )  ||  /  /  /
@@ -22,38 +38,70 @@ def path():
  |   |/     \     ||  /  S  /   /_||/__| |__\   
 _______|___|_________/  T  /__________________      
 < -- V I L L A G E            D E S E R T -- >
-----------------------------------------------
+----------------------------------------------''')
+        incorrect_path = True   # used while incorrect paths are chosen
+        while incorrect_path:
+            path = input('''
 1. Will you go to the village?
 2. Do you choose the sneaky forest?
 3. Are you confident enough to be exposed in the dessert? 
 Choose your destiny! --> ''')
-    if path == "1":
-        print("""You are walking into the village
+            if path == "1":
+                incorrect_path = False
+                print("""You are walking into the village                    
 From a backside alley an enemy appears...""")
-    elif path == "2":
-        print("""The darkness of the forest awaits
+            elif path == "2":
+                incorrect_path = False
+                print("""The darkness of the forest awaits
 Two steps into the forest you hear something in the bushes...
 You go take a closer look...""")
-    elif path == "3":
-        print("""The open desert lies ahead.
+            elif path == "3":
+                incorrect_path = False
+                print("""The open desert lies ahead.
 Something breathes behind you...
 You turn around...""")
-    else:
-        print("Path not available")
-    return path
+            else:
+                incorrect_path = True
+                print("""Path not available.
+Please try again: """)
+        return path
 
-@staticmethod
-def random_number():
-    number = random.randint(0, 2)
-    sound = "BattleFinal.wav"
-    winsound.PlaySound(sound, winsound.SND_ASYNC)
-    if number == 0:
-        enemy = Enemies.Goblin()
-        input("Press any key to continue")
-    elif number == 1:
-        enemy = Enemies.Rat()
-        input("Press any key to continue")
-    elif number == 2:
-        enemy = Enemies.Orc()
-        input("Press any key to continue")
-    return enemy
+
+class Actions:
+    @staticmethod
+    def random_number():
+        number = random.randint(0, 2)
+        sound = "BattleFinal.wav"
+        winsound.PlaySound(sound, winsound.SND_ASYNC)
+        if number == 0:
+            enemy = Enemies.Goblin()
+            input("Press any key to continue")
+        elif number == 1:
+            enemy = Enemies.Rat()
+            input("Press any key to continue")
+        elif number == 2:
+            enemy = Enemies.Orc()
+            input("Press any key to continue")
+        return enemy
+
+    @staticmethod
+    def fight(player, enemy):
+        os.system("cls")
+        while player.health > 0 and enemy.health > 0:
+            player.health -= enemy.damage
+            enemy.health -= (player.mana + player.damage)
+        if enemy.health == 0:
+            print(f"""Congratulations, 
+You have won your first battle!
+And with {player.health} energy to spare! Ha! Ha! Ha!""")
+            input("Press any key to continue")
+            os.system("cls")
+        elif player.health == 0:
+            print("The enemy has proven to be stronger than you! Try again!")
+            input("Press any key to continue")
+            os.system("cls")
+
+
+
+
+
--- main.py
+++ main.py
@@ -5,43 +5,55 @@ import Enemies
 import Utils
 import random
 
-introMsg = """ 
-*****************************************************************************************
-* Welcome, stranger!                                                                    *
-* Here in Hinterlands, you'll get to fight dragons and conquer the deadliest dungeons.  *
-* In a country where magic rules, anything is possible if you wish so.                  *
-* It all depends on you, brave hero.                                                    *
-*****************************************************************************************
-"""
-
-
 sound = "Main_Menu.wav"
 winsound.PlaySound(sound, winsound.SND_ASYNC)
 
-print(introMsg)
-user_answer = input(''' Would you like to start the adventure?
+Utils.Messages.intro_message()
+play = True  # used wile player does not exit
+while play:
+    user_answer = input('''Would you like to start the adventure?
 Yes or No -> ''')
-if user_answer.upper() == "YES":
-    print("OK")
-    os.system("cls")  # windows
-    choice = input('''Choose your class:
+    if user_answer.upper() == "YES":
+        print("OK")
+        os.system("cls")  # windows
+        incorrect_choice = True  # used while incorrect choices are made
+        while incorrect_choice:
+            choice = input('''Choose your class:
 1 for Warrior, 2 for Wizard --> ''')
-    if choice == "1":
-        player_name = input('''You have chosen o be a mighty warrior!
+            if choice == "1":
+                incorrect_choice = False
+                player_name = input('''You have chosen to be a mighty warrior!
 What is your name? --> ''')
-        player = Player.Warrior(player_name)
-        Utils.path()
-        Utils.random_number()
+                player = Player.Warrior(player_name)
+                Utils.Choices.path()
+                enemy = Utils.Actions.random_number()
+                Utils.Actions.fight(player, enemy)
+                print("""Thank you for playing!
+Good luck in your next adventures!
+Farewell!""")
+                play = False
 
-    elif choice == "2":
-        player_name = input('''You have chosen o be a mighty wizard!
+            elif choice == "2":
+                incorrect_choice = False
+                player_name = input('''You have chosen to be a mighty wizard!
 What is your name? --> ''')
-        player = Player.Wizard(player_name)
-        Utils.path()
-        Utils.random_number()
+                player = Player.Wizard(player_name)
+                Utils.Choices.path()
+                enemy = Utils.Actions.random_number()
+                Utils.Actions.fight(player, enemy)
+                print("""Thank you for playing!
+Good luck in your next adventures!
+Farewell!""")
+                play = False
 
+            else:
+                incorrect_choice = True
+                print("That is not a valid option! Please try again!")
+
+    elif user_answer.upper() == "NO":
+        play = False
+        print("Thank you, good bye!")
     else:
-        print("That is not a valid option!")
+        print("That is not a valid option. Please try again!")
+        play = True
 
-else:
-    print("Thank you, good bye!")
